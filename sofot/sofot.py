92ccd029f0d3a5ede8312ce772bb5e32222620cf
campovski <campovski@gmail.com>
2021-02-20 16:17:42 +0100

    Compare detections with ground truth, include error

--- sofot/sofot.py
+++ sofot/sofot.py
@@ -30,6 +30,7 @@ class Sofot():
     self._bbox_color = numpy.random.randint(0, 255, (len(self.bboxes), 3))
 
   def track(self):
+    print(f"[Sofot.track] started tracking video {self.video}")
     # Read starting frame and convert it to grayscale.
     frame = util.img_for_video_frame(self.video, self.current_frame)
     self.__frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -45,12 +46,19 @@ class Sofot():
     # Instatiate a mask for displaying optical flow on the image.
     self.__disp_flow_mask = numpy.zeros_like(frame)
 
+    # Error is increased when a detection does not match a ground truth box.
+    self.error = 0
+    self.frame_with_first_error = None
+
     # Main loop.
     while self.current_frame < self.nframes:
       self._step()
 
     # Close all windows at the end.
     cv2.destroyAllWindows()
+
+    # Print errors to the console.
+    print(f"[Sofot.track] finished with error {self.error}" + (f" and first frame with error {self.frame_with_first_error}" if self.error > 0 else ""))
 
   def _step(self):
     # Go to next frame.
@@ -96,57 +104,29 @@ class Sofot():
       bboxes_new.append(bbox)
 
     # Create pairs of bounding boxes.
-    pairs = []
-    for i_old, bb_old in enumerate(self.bboxes):
-      match = None
-
-      # Calculate overlap of bounding boxes.
-      iou_max = 0.0
-      for i_new, bb_new in enumerate(bboxes_new):
-        iou = util.iou(bb_old, bb_new)
-        if iou > iou_max:
-          iou_max = iou
-          match = i_new
-
-      if match is not None:
-        pairs.append((i_old, match))
-      
-    # Check for missing.
-    if len(pairs) < len(self.bboxes):
-      matched_old = [x[0] for x in pairs]
-      matched_new = [x[1] for x in pairs]
-      for i_old, bb_old in enumerate(self.bboxes):
-        if i_old in matched_old:
-          continue
-
-        match = None
-        d_min = numpy.Inf
-        for i_new, bb_new in enumerate(bboxes_new):
-          # Skip already matched new bounding boxes.
-          if i_new in matched_new:
-            continue
-
-          d = util.bbox_distance(bb_old, bb_new)
-          if d < d_min:
-            d_min = d
-            match = i_new
-
-        if match is not None:
-          pairs.append((i_old, match))
-          matched_old.append(i_old)
-
-      # Update colors if still not all matched.
-      if len(pairs) < len(self.bboxes):
-        colors_new = []
-        for ci in [x[0] for x in sorted(pairs, key=lambda x: x[1])]:
-          colors_new.append(self._bbox_color[ci])
-        self._bbox_color = numpy.array(colors_new)
+    pairs = self._pair_bboxes(self.bboxes, bboxes_new, change_colors=True)
 
     if self._debug:
       print(self._bbox_color)
       print(pairs)
 
-    # TODO Compare bounding boxes with ground truth using pairs.
+    # Compare bounding boxes against ground truth. It is ok, if a bounding box is in ground truth
+    # and not in detections, however every detection must have a corresponding bounding box.
+    i_annot = self.current_frame - 1
+    bboxes_ground_truth = [[(bb[0], bb[1]), (bb[2], bb[3])] for bb in self.annotations[i_annot]]
+    pairs_ground_truth = self._pair_bboxes(bboxes_new, bboxes_ground_truth)
+    diff = len(pairs) - len(pairs_ground_truth)
+    if diff > 0:  # cannot be negative
+      self.error += diff
+      if self.frame_with_first_error is None:
+        self.frame_with_first_error = self.current_frame
+
+    if self._debug:
+      print(pairs_ground_truth)
+      print("error", self.error)
+
+    # Write error on screen.
+    img = cv2.putText(img, f"Error: {self.error}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, color=(0, 0, 255), thickness=1)
 
     # Display image.
     cv2.imshow("SOFOT", img)
@@ -156,3 +136,52 @@ class Sofot():
     self.__frame_gray = frame_gray.copy()
     self.of_points = of_points_new
     self.bboxes = bboxes_new
+
+  def _pair_bboxes(self, bboxes_true, bboxes_new, change_colors=False):
+    pairs = []
+    for i_true, bb_true in enumerate(bboxes_true):
+      match = None
+
+      # Calculate overlap of bounding boxes.
+      iou_max = 0.0
+      for i_new, bb_new in enumerate(bboxes_new):
+        iou = util.iou(bb_true, bb_new)
+        if iou > iou_max:
+          iou_max = iou
+          match = i_new
+
+      if match is not None:
+        pairs.append((i_true, match))
+      
+    # Check for missing.
+    if len(pairs) < len(bboxes_true):
+      matched_true = [x[0] for x in pairs]
+      matched_new = [x[1] for x in pairs]
+      for i_true, bb_true in enumerate(bboxes_true):
+        if i_true in matched_true:
+          continue
+
+        match = None
+        d_min = numpy.Inf
+        for i_new, bb_new in enumerate(bboxes_new):
+          # Skip already matched new bounding boxes.
+          if i_new in matched_new:
+            continue
+
+          d = util.bbox_distance(bb_true, bb_new)
+          if d < d_min:
+            d_min = d
+            match = i_new
+
+        if match is not None:
+          pairs.append((i_true, match))
+          matched_true.append(i_true)
+
+      # Update colors if still not all matched.
+      if change_colors and len(pairs) < len(bboxes_true):
+        colors_new = []
+        for ci in [x[0] for x in sorted(pairs, key=lambda x: x[1])]:
+          colors_new.append(self._bbox_color[ci])
+        self._bbox_color = numpy.array(colors_new)
+
+    return pairs
